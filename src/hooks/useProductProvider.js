911ad694d3252f577e3ad3bdb15cfe3219b2a9e7
José Costa <josecostasantos.jr@gmail.com>
2022-11-08 02:38:39 -0300

    refatorando busca

--- src/hooks/useProductProvider.js
+++ src/hooks/useProductProvider.js
@@ -6,7 +6,16 @@ function useProductProvider() {
     const [fruits, setFruits] = useState([]);
     const [inputValue, setInputValue] = useState('');
 
-    const filtered = fruits.filter(item => item.name.includes(inputValue));
+    // const filtered = fruits.filter(item => item.name.includes(inputValue));
+    const filtered = fruits.filter(item => item.name.toUpperCase().includes(inputValue.toUpperCase()));
+
+    // const filtered = fruits.filter((item) => {
+    //     const name = item.name.toUpperCase()
+    //     if (name.includes(inputValue.toUpperCase())) {
+    //         return name
+    //     }
+    //     return false
+    // });
 
     useEffect(() => {
         setFruits([...dbFruits]);
